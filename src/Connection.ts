8d673292f5ba1563d3a0e7a40d92244861fc1c63
Akseli Koskinen <koskinen.akseli@gmail.com>
2018-10-29 23:10:29 +0200

    Connection class

---
+++ src/Connection.ts
@@ -0,0 +1,31 @@
+import io from 'socket.io-client';
+
+const isWSUrl = /^(ws:\/\/)?([a-z]+\.)*([a-z]+)(:[0-9]+)?\/?$/i;
+
+export default class Connection {
+  private conn: SocketIOClient.Socket;
+
+  constructor(host: string) {
+    if (!host) {
+      throw this.usage();
+    }
+    if (!host.match(isWSUrl)) {
+      throw this.usage();
+    }
+    this.conn = io(host);
+    this.registerDefaultEvents();
+  }
+
+  private usage() {
+    console.error('Usage: siocli hostname[:port]');
+  }
+
+  private registerDefaultEvents() {
+    this.conn.on('error', (error: string) => {
+      console.error(error);
+    });
+    this.conn.on('connect', () => {
+      console.log('connected successfully');
+    });
+  }
+}
