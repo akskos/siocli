1fab39f2253dabe708af7e2e53f9396f0e3f0ae0
Akseli Koskinen <koskinen.akseli@gmail.com>
2018-11-04 18:37:28 +0200

    remove unnecessary stuff

--- src/Main.ts
+++ src/Main.ts
@@ -1,26 +1,16 @@
-import Connection from './Connection';
 import { Commands } from './commands';
 import InputController from './InputController';
-import Config from './Config';
 
 export default class Main {
   private ic: InputController;
-  private conn: Connection;
   constructor(
     inputController: InputController,
-    private config: Config,
     private commands: Commands) {
     this.ic = inputController;
-    this.conn = this.initSocketIOConnection();
   }
 
   public async start() {
     await this.cliUiLoop();
-  }
-
-  private initSocketIOConnection() {
-    const url = this.config.url;
-    return new Connection(url);
   }
 
   private async cliUiLoop() {
